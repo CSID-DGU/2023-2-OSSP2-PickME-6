40a0abd8191d1d7b1f67efe445ae26e8b19e79c0
kyungmin00 <ekfkawnl20202@gmail.com>
2023-11-23 23:14:52 +0900

    매칭페이지 수정

--- lib/pages/Match_page.dart
+++ lib/pages/Match_page.dart
@@ -8,14 +8,34 @@ class MatchingPage extends StatefulWidget {
 }
 
 class _MatchingState extends State<MatchingPage> {
-  String selectedCategory = '한식'; // 한식으로 보이게 설정
-  String selectedFood = '';
+  late String selectedCategory;
+  late String selectedFood;
+  late List<String> foods;
+  late Map<String, List<String>> categoryToFoodMap;
+  late List<String> categories;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCategory = '한식'; //기본설정
+    categoryToFoodMap = { // 더 추가하기
+      '한식': ['삼겹살', '국밥', '설렁탕'],
+      '중식': ['짜장면', '짬뽕', '탕수육', '마라탕'],
+      '일식': ['초밥', '라멘'],
+      '양식': ['햄버거', '파스타'],
+      '분식': ['김밥', '떡볶이'],
+      '기타': ['커피', '간식'],
+    };
+    categories = categoryToFoodMap.keys.toList();
+    foods = categoryToFoodMap[selectedCategory] ?? [];
+    selectedFood = foods.isNotEmpty ? foods.first : '';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Matching'),
+        title: SizedBox.shrink(),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -32,9 +52,20 @@ class _MatchingState extends State<MatchingPage> {
               onChanged: (String? newValue) {
                 setState(() {
                   selectedCategory = newValue!;
+                  foods = categoryToFoodMap[selectedCategory] ?? [];
+                  selectedFood = foods.isNotEmpty ? foods.first : '';
                 });
-              }, // 드롭다운버튼 사용 (카테고리 선택)
-              items: <String>['한식', '중식', '일식', '양식'].map<DropdownMenuItem<String>>((String value) {
+              },
+              style: TextStyle(fontSize: 18, color: Colors.black),
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 36,
+              underline: Container(
+                height: 2,
+                color: Colors.blue,
+              ),
+              elevation: 8,
+              isExpanded: true,
+              items: categories.map((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -43,39 +74,42 @@ class _MatchingState extends State<MatchingPage> {
             ),
             SizedBox(height: 20),
             Text(
-              '희망 주문 메뉴를 적어주세요',
+              '희망 주문 메뉴를 선택하세요',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey),
+            DropdownButton<String>(
+              value: selectedFood,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedFood = newValue!;
+                });
+              },
+              style: TextStyle(fontSize: 18, color: Colors.black),
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 36,
+              underline: Container(
+                height: 2,
+                color: Colors.blue,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      selectedFood = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: '메뉴를 입력하세요',
-                  ),
-                ),
-              ),
+              elevation: 8,
+              isExpanded: true,
+              items: foods.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             Spacer(),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  // 버튼 누르고 이후 코드 작성 필요
+                  // 버튼 누른 이후 코드
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF2B4177), // 버튼색
+                  backgroundColor: Color(0xFF2B4177),
                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                 ),
                 child: Padding(

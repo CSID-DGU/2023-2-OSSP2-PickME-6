6c058cf9cdd2da7097553801c81de03bab3fdc4e
Limchihyeon <cse.lyc3004@gmail.com>
2023-11-29 15:10:25 +0900

    채팅방 프로필 이미지 출력

--- lib/chatting/chat/chat_bubble.dart
+++ lib/chatting/chat/chat_bubble.dart
@@ -1,69 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_bubble/bubble_type.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
-import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_6.dart';
+import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_3.dart';
 class ChatBubbles extends StatelessWidget {
-  const ChatBubbles(this.message, this.isMe, this.userName, {Key? key}):super(key:key);
+  const ChatBubbles(this.message, this.isMe, this.userName, this.userImage, {Key? key}):super(key:key);
 
   final String message;
   final bool isMe;
   final String userName;
+  final String userImage;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe
-          ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        if(isMe)
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0,0,3,0),
-            child:
-            Column(
-              children: [
-                Text(
-                  userName,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold, color:  Colors.black,
-                  )
-                ),
-                ChatBubble(
-                    clipper: ChatBubbleClipper3(type: BubbleType.sendBubble),
-                    alignment: Alignment.topRight,
-                    margin: EdgeInsets.only(top: 20),
-                    backGroundColor: Colors.blue,
-                    child: Container(
-                      constraints: BoxConstraints(
-                        maxWidth: MediaQuery.of(context).size.width * 0.7,
+        Row(
+          mainAxisAlignment: isMe
+              ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            if(isMe)
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,10,45,0),
+                child:
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      userName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500, color:  Colors.black,
+                      )
+                    ),
+                    ChatBubble(
+                        clipper: ChatBubbleClipper3(type: BubbleType.sendBubble),
+                        alignment: Alignment.topRight,
+                        margin: EdgeInsets.only(top: 20),
+                        backGroundColor: Colors.blue,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.7,
+                          ),
+                          child: Text(
+                            message,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
                       ),
-                      child: Text(
-                        message,
-                        style: TextStyle(color: Colors.white),
+                  ],
+                ),
+                ),
+            if(!isMe)
+              Padding(
+                padding: const EdgeInsets.fromLTRB(45,10,0,0),
+                child:
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                        userName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold, color:  Colors.black,
+                        )
+                    ),
+                    ChatBubble(
+                      clipper: ChatBubbleClipper3(type: BubbleType.receiverBubble),
+                      backGroundColor: Color(0xffE7E7ED),
+                      margin: EdgeInsets.only(top: 20),
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.7,
+                        ),
+                        child: Text(
+                          message,
+                          style: TextStyle(color: Colors.black),
+                        ),
                       ),
                     ),
-                  ),
-              ],
-            ),
-            ),
-        if(!isMe)
-          Padding(
-            padding: const EdgeInsets.fromLTRB(3,0,0,0),
-            child: ChatBubble(
-              clipper: ChatBubbleClipper3(type: BubbleType.receiverBubble),
-              backGroundColor: Color(0xffE7E7ED),
-              margin: EdgeInsets.only(top: 20),
-              child: Container(
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                  ],
                 ),
-                child: Text(
-                  message,
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-          )
-      ],
+              )
+          ],
+        ),
+        Positioned(
+          top: 0,
+          right: isMe ? 5 : null,
+          left: isMe ? null : 5,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(userImage),
+          ),
+        )
+      ]
     );
   }
 }

82c927fe7c13947cd896e0e6c21cce0ae6a79b3b
gitsuhyun <suhyun10201@naver.com>
2023-12-02 13:21:32 +0900

    fix: 초기화면 상태 원상복구

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
                primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'PickME Home Page'),
-      //home: const LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
